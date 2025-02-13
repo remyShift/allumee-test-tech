1e0988263f5da403ac4ba649a1c7e224ed712f90
Remy Cassagne <remy.engassac@icloud.com>
2025-02-13 14:25:38 +0100

    throw and error if miss one of scene

--- src/__tests__/models/Transition.test.ts
+++ src/__tests__/models/Transition.test.ts
@@ -10,6 +10,12 @@ describe("Transition", () => {
         expect(transition.name).toBe(`${scene1.name} > ${scene2.name}`);
     });
 
+    it("should throw an error if the name of one of the scene is not provided", () => {
+        const scene1 = new Scene('Torche', 30);
+        
+        expect(() => new Transition(scene1.name, '')).toThrow('Need 2 scenes to create a transition');
+    });
+
     it("should create a transition with a default duration of 10 seconds", () => {
         const scene1 = new Scene('Torche', 30);
         const scene2 = new Scene('Torche 2', 30);
