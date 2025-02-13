984792a3235d7e30a597b7863bc83068f16dbe7d
Remy Cassagne <remy.engassac@icloud.com>
2025-02-13 14:22:20 +0100

    initialize transition with a default duration

--- src/__tests__/models/Transition.test.ts
+++ src/__tests__/models/Transition.test.ts
@@ -9,4 +9,12 @@ describe("Transition", () => {
 
         expect(transition.name).toBe(`${scene1.name} > ${scene2.name}`);
     });
+
+    it("should create a transition with a default duration of 10 seconds", () => {
+        const scene1 = new Scene('Torche', 30);
+        const scene2 = new Scene('Torche 2', 30);
+        const transition = new Transition(scene1.name, scene2.name);
+
+        expect(transition.duration).toBe(10);
+    });
 });
--- src/models/Transition.ts
+++ src/models/Transition.ts
@@ -1,5 +1,7 @@
 export default class Transition {
-    constructor() {
-        this.name = name;
+    name: string;
+
+    constructor(scene1Name: string, scene2Name: string) {
+        this.name = `${scene1Name} > ${scene2Name}`;
     }
 }
