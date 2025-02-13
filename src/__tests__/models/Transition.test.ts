f06eeb70452d274ef0040d5021e058becad7b4f2
Remy Cassagne <remy.engassac@icloud.com>
2025-02-13 14:21:05 +0100

    initialize transition with name of 2 scene

--- src/__tests__/models/Transition.test.ts
+++ src/__tests__/models/Transition.test.ts
@@ -1,7 +1,12 @@
 import Transition from "@/models/Transition";
+import Scene from "@/models/Scene";
 
 describe("Transition", () => {
-    it("should create a transition", () => {
-        const transition = new Transition();
+    it("should create a transition with a name corresponding to the name of 2 scenes", () => {
+        const scene1 = new Scene('Torche', 30);
+        const scene2 = new Scene('Torche 2', 30);
+        const transition = new Transition(scene1.name, scene2.name);
+
+        expect(transition.name).toBe(`${scene1.name} > ${scene2.name}`);
     });
 });
---
+++ src/models/Transition.ts
@@ -0,0 +1,5 @@
+export default class Transition {
+    constructor() {
+        this.name = name;
+    }
+}
