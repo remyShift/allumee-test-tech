20aff270d75ae95b5a9a70e73df19376c2263416
Remy Cassagne <remy.engassac@icloud.com>
2025-02-13 14:30:44 +0100

    refacto transition test

---
+++ src/__tests__/models/Show.test.ts
@@ -0,0 +1,7 @@
+import Show from "@/models/Show";
+
+describe("Show", () => {
+    it("should create a show", () => {
+        const show = new Show();
+    });
+});
--- src/__tests__/models/Transition.test.ts
+++ src/__tests__/models/Transition.test.ts
@@ -2,11 +2,17 @@ import Transition from "@/models/Transition";
 import Scene from "@/models/Scene";
 
 describe("Transition", () => {
-    it("should create a transition with a name corresponding to the name of 2 scenes", () => {
-        const scene1 = new Scene('Torche', 30);
-        const scene2 = new Scene('Torche 2', 30);
-        const transition = new Transition(scene1.name, scene2.name);
+    let transition: Transition;
+    let scene1: Scene;
+    let scene2: Scene;
 
+    beforeEach(() => {
+        scene1 = new Scene('Torche', 30);
+        scene2 = new Scene('Torche 2', 30);
+        transition = new Transition(scene1.name, scene2.name);
+    });
+
+    it("should create a transition with a name corresponding to the name of 2 scenes", () => {
         expect(transition.name).toBe(`${scene1.name} > ${scene2.name}`);
     });
 
@@ -17,18 +23,10 @@ describe("Transition", () => {
     });
 
     it("should create a transition with a default duration of 10 seconds", () => {
-        const scene1 = new Scene('Torche', 30);
-        const scene2 = new Scene('Torche 2', 30);
-        const transition = new Transition(scene1.name, scene2.name);
-
         expect(transition.duration).toBe(10);
     });
 
     it("should update the duration of the transition", () => {
-        const scene1 = new Scene('Torche', 30);
-        const scene2 = new Scene('Torche 2', 30);
-        const transition = new Transition(scene1.name, scene2.name);
-
         transition.updateDuration = 20;
 
         expect(transition.duration).toBe(20);
