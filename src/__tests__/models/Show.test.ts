ab0cc6d4c5e81617f3f25d62eca72fe56c961915
Remy Cassagne <remy.engassac@icloud.com>
2025-02-13 14:35:55 +0100

    initialize show empty

--- src/__tests__/models/Show.test.ts
+++ src/__tests__/models/Show.test.ts
@@ -1,7 +1,12 @@
 import Show from "@/models/Show";
 
 describe("Show", () => {
-    it("should create a show", () => {
+    it("should create a show with empty scenes and transitions and so empty events and duration 0", () => {
         const show = new Show();
+
+        expect(show.scenes).toEqual([]);
+        expect(show.transitions).toEqual([]);
+        expect(show.events).toEqual([]);
+        expect(show.duration).toEqual(0);
     });
 });
