014d2da0f457cfe4c19bfd9f06bc6d009a0fae5d
Remy Cassagne <remy.engassac@icloud.com>
2025-02-13 14:45:05 +0100

    add 2 scenes to a show

--- src/__tests__/models/Show.test.ts
+++ src/__tests__/models/Show.test.ts
@@ -1,5 +1,6 @@
 import Scene from "@/models/Scene";
 import Show from "@/models/Show";
+import Transition from "@/models/Transition";
 
 describe("Show", () => {
     it("should create a show with empty scenes and transitions and so empty events and duration 0", () => {
@@ -11,7 +12,7 @@ describe("Show", () => {
         expect(show.duration).toEqual(0);
     });
 
-    it("should add a scene to the show", () => {
+    it("should add a scene to the show and update the duration", () => {
         const show = new Show();
         const scene = new Scene('Torche', 30);
 
@@ -21,5 +22,19 @@ describe("Show", () => {
         expect(show.duration).toEqual(30);
         expect(show.transitions).toEqual([]);
         expect(show.events).toEqual([scene]);
+    });
+
+    it("should add 2 scenes to the show and put a transition between them and update the duration", () => {
+        const show = new Show();
+        const scene1 = new Scene('Torche', 30);
+        const scene2 = new Scene('Torche 2', 30);
+
+        show.addScene(scene1);
+        show.addScene(scene2);
+
+        expect(show.scenes).toEqual([scene1, scene2]);
+        expect(show.duration).toEqual(70);
+        expect(show.transitions).toEqual([new Transition(scene1.name, scene2.name)]);
+        expect(show.events).toEqual([scene1, new Transition(scene1.name, scene2.name), scene2]);
     });
 });
--- src/models/Show.ts
+++ src/models/Show.ts
@@ -4,7 +4,7 @@ import Transition from "./Transition";
 export default class Show {
     scenes: Scene[];
     transitions: Transition[];
-    events: Event[];
+    events: (Scene | Transition)[];
     duration: number;
 
     constructor() {
@@ -12,5 +12,20 @@ export default class Show {
         this.transitions = [];
         this.events = [];
         this.duration = 0;
+    }
+
+    addScene(newScene: Scene) {
+        if (this.scenes.length > 0) {
+            const lastScene = this.scenes[this.scenes.length - 1];
+            const transition = new Transition(lastScene.name, newScene.name);
+
+            this.transitions.push(transition);
+            this.events.push(transition);
+            this.duration += transition.duration;
+        }
+
+        this.scenes.push(newScene);
+        this.duration += newScene.duration;
+        this.events.push(newScene);
     }
 }
